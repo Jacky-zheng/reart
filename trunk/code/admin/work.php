7f0dcc7b1593b95a702d6f2a52b10e9e0d587f66
hayley.fc <hayley.fc@9714652a-0c74-11de-97d8-e990b0f60ea0>
2009-04-08 12:59:32 +0000

    二期中对后台修改

--- trunk/code/admin/work.php
+++ trunk/code/admin/work.php
@@ -18,10 +18,7 @@ $sClass = "category";
 $tpl->assign("PAGE_FUNC_BIG_LINK", "work.php");
 $tpl->assign("PAGE_FUNC_BIG_NAME", "作品管理");
 $tpl->assign("aENUM",$aENUM);
-
-$aOptions = artist::getArtistList();
-
-if($sAction == "add") //添加
+if ($sAction == "add") //添加
 {	
 	$cID = isset($_GET['cID']) ? $_GET['cID'] : 1;
 	$oFCKeditor = new FCKeditor('FCKeditor1') ; 
@@ -50,7 +47,7 @@ if($sAction == "add") //添加
 	$tpl->assign("cID",$cID);		
 	$tpl->display("admin/work_add.tpl.htm");
 }
-elseif($sAction=="addSave") // 添加保存
+elseif ($sAction=="addSave") // 添加保存
 {
 	if (empty($_POST['name']))
 	{
@@ -95,50 +92,89 @@ elseif($sAction=="addSave") // 添加保存
 		redirect("work.php?act=add&cID=".$_POST['cID'],2,"添加失败！");
 	}	
 }
-elseif($sAction == "edit" && isset($_GET['id'])) // 修改
+elseif($sAction == "edit") // 修改
 {	
-	$id = $_GET['id'];
-	$tpl->assign("PAGE_FUNC_SMALL_NAME","编辑作品");	
-	$sSQL = "SELECT * FROM $sTbl WHERE id=$id";
-	$aField = $db->getRecordSet($sSQL,1);		
-	$oFCKeditor = new FCKeditor('FCKeditor1') ; 
-	$oFCKeditor->BasePath = "../js/editor/" ; 
-	$oFCKeditor->ToolbarSet = "Default" ; 
-	$oFCKeditor->InstanceName = "content" ; 
-	$oFCKeditor->Width = "90%" ; 
-	$oFCKeditor->Height = "400" ; 
-	$oFCKeditor->Value = $aField['description'];
-	$sContent = $oFCKeditor->Create() ; 
-	$tpl->assign("sContent",$sContent);	
-
-	$eFCKeditor = new FCKeditor('FCKeditor1') ; 
-	$eFCKeditor->BasePath = "../js/editor/" ; 
-	$eFCKeditor->ToolbarSet = "Default" ; 
-	$eFCKeditor->InstanceName = "econtent" ; 
-	$eFCKeditor->Width = "90%" ; 
-	$eFCKeditor->Height = "400" ;  
-	$eFCKeditor->Value = $aField['edescription'];
-	$eContent = $eFCKeditor->Create() ; 
-	$tpl->assign("eContent",$eContent);
-
+	if (isset($_GET['id']) && $_GET['id']>0)
+	{
+		$flag = '1';//编辑作品
+		$id = $_GET['id'];
+		$tpl->assign("PAGE_FUNC_SMALL_NAME","编辑作品");	
+		$sSQL = "SELECT * FROM $sTbl WHERE id=$id";
+		$aField = $db->getRecordSet($sSQL,1);
+		$cid = $aField['cID'];
+		$artarr =  explode(',', $aField['artistCode']);
+		$aid = $artarr[0];
+		foreach ($artarr as $k=>$v)
+		{
+			$ainfo = artist::getArtistDetail($v);
+			$aField['artlist'] .= "<table id='tbl_".$v."' style='float:left'><tbody><tr><td>".$ainfo['name']."</td><td><img src='/images/f2.gif' alt='删除' onclick='delArtist(".$v.");'/>;</td></tr></tbody></table>";
+		}
+	}
+	else 
+	{
+		$tpl->assign("PAGE_FUNC_SMALL_NAME","添加作品");	
+		$cid = isset($_GET['cID']) ? $_GET['cID'] : 1;;
+		$aid = 0;
+		$aField['artlist'] = '';
+	}		
+	{
+		$sDescription = createEditor('description', $aField['description']) ; 
+		$tpl->assign("sDescription",$sDescription);	
+		
+		$eDescription = createEditor('edescription', $aField['edescription']) ; 
+		$tpl->assign("eDescription",$eDescription);		
+	}
+	{
+		$sComment = createEditor('comment', $aField['comment']) ; 
+		$tpl->assign("sComment",$sComment);	
+		
+		$eComment = createEditor('ecomment', $aField['ecomment']) ; 
+		$tpl->assign("eComment",$eComment);		
+	}
+	{
+		$sAttachment = createEditor('attachment', $aField['attachment']) ; 
+		$tpl->assign("sAttachment",$sAttachment);	
+		
+		$eAttachment = createEditor('eattachment', $aField['eattachment']) ; 
+		$tpl->assign("eAttachment",$eAttachment);		
+	}
 	// 栏目
-	$sOptions = category::getCatOptions($sClass,0,$aField['cID']);	// -1表示无限级分类； 0表示一级分类； 1表示二级分类；以此类推
+	$sOptions = category::getCatOptions($sClass,0,$cid);	// -1表示无限级分类； 0表示一级分类； 1表示二级分类；以此类推
 	$tpl->assign("sOptions",$sOptions);
+	//显示作者
+	$aOptions = artist::getArtistList($aid);
+	$tpl->assign("aOptions",$aOptions);
 	
 	$tpl->assign("aField",$aField);
 	$tpl->display("admin/work_edit.tpl.htm");
 }
-elseif ($sAction == "editSave" && isset($_POST['id'])) // 修改保存
+elseif ($sAction == "editSave") // 修改保存
 {	
 	$id = $_POST['id'];
 	$aField['cID'] = $_POST['cID'];
 	$aField['name'] = $_POST['name'];
 	$aField['ename'] = $_POST['ename'];
-	$aField['price'] = $_POST['price'];
-	$aField['age'] = $_POST['age'];
-	$aField['picCode'] = $_POST['picCode'];
 	$aField['artistCode'] = $_POST['artistCode'];
+	$aField['price'] = $_POST['price'];
+	$aField['actualPrice'] = $_POST['actualPrice'];
+	$aField['age'] = $_POST['age'];
+	$aField['texture'] = $_POST['texture'];
+	$aField['etexture'] = $_POST['etexture'];
 	$aField['size'] = $_POST['size'];
+	$aField['picCode'] = $_POST['picCode'];
+	$aField['rank'] = $_POST['rank'];
+	$aField['status'] = $_POST['status'];
+	
+	$aField['comment'] = $_POST['comment'];
+	$aField['ecomment'] = $_POST['ecomment'];
+	$aField['description'] = $_POST['description'];
+	$aField['edescription'] = $_POST['edescription'];
+	
+	$aField['attachment'] = $_POST['attachment'];
+	$aField['eattachment'] = $_POST['eattachment'];
+		
+	$aField['addDate'] = date("Y-m-d H:i:s");
+	/*
 	$aField['esize'] = $_POST['esize'];
 	$aField['signal'] = $_POST['signal'];
 	$aField['esignal'] = $_POST['esignal'];
@@ -156,17 +192,36 @@ elseif ($sAction == "editSave" && isset($_POST['id'])) // 修改保存
 	$aField['etexture'] = $_POST['etexture'];
 	$aField['edescription'] = $_POST['econtent'];
 	$aField['addDate'] = date("Y-m-d H:i:s");
-
-	if($db->update($sTbl,$aField,"id=$id"))
+	*/
+	if ($id > 0)
 	{
-		/*作品首页推荐，则更新首页flash的xml文件*/
-		refreshIndex();
-		redirect("work.php?act=listAll"/*&cID=".$_POST['cID']*/,3,"修改成功！");	
+		if($db->update($sTbl,$aField,"id=$id"))
+		{
+			/*作品首页推荐，则更新首页flash的xml文件*/
+			refreshIndex();
+			redirect("work.php?act=listAll"/*&cID=".$_POST['cID']*/,3,"修改成功！");	
+		}
+		else 
+		{
+			redirect("work.php?act=edit&id=".$id,3,"修改失败！");
+		}	
 	}
 	else 
 	{
-		redirect("work.php?act=edit&id=".$id,3,"修改失败！");
-	}	
+		if($db->insert($sTbl,$aField))
+		{
+			/*作品首页推荐，则更新首页flash的xml文件*/
+			if ($aField['status'] == '1')
+			{
+				refreshIndex();
+			}
+			redirect("work.php?act=edit&cID=".$_POST['cID'],2,"添加成功！");
+		}
+		else 
+		{
+			redirect("work.php?act=edit&cID=".$_POST['cID'],2,"添加失败！");
+		}		
+	}
 }
 elseif ($sAction == "listAll")  // 列表
 {
@@ -315,5 +370,18 @@ function refreshIndex()
 	$str = file_get_contents("../fx_config.xml");
 	$str = str_replace("detail.php?id", "detail.php?language=en&id", $str);
 	file_put_contents("../fx_config_en.xml", $str);
+}
+
+function createEditor($instance_name, $value)
+{
+	$oFCKeditor = new FCKeditor('FCKeditor1') ; 
+	$oFCKeditor->BasePath = "../js/editor/" ; 
+	$oFCKeditor->ToolbarSet = "Default" ; 
+	$oFCKeditor->InstanceName = $instance_name ; 
+	$oFCKeditor->Width = "90%" ; 
+	$oFCKeditor->Height = "200" ; 
+	$oFCKeditor->Value = $value;
+	$sContent = $oFCKeditor->Create() ; 
+	return $sContent;
 }
 ?>

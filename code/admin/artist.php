6d63371fef035f6f7d8b52cc6ef4a64e50828f23
hayley.fc <hayley.fc@9714652a-0c74-11de-97d8-e990b0f60ea0>
2009-04-08 12:59:32 +0000

    二期中对后台修改

--- code/admin/artist.php
+++ code/admin/artist.php
@@ -6,7 +6,7 @@
 require_once("check_login.php"); //登录检查
 require_once("../class/common.inc.php");
 include "../js/editor/fckeditor.php";
-
+loadLib("artist");
 $sAction = (isset($_GET["act"])) ? $_GET["act"] : "listAll";
 
 $sTbl =  "artist";
@@ -65,12 +65,20 @@ elseif($sAction=="addSave") // 添加保存
 		redirect("artist.php?act=add",2,"添加失败！");
 	}	
 }
-elseif($sAction == "edit" && isset($_GET['id'])) // 修改
+elseif($sAction == "edit") // 修改
 {	
 	$id = $_GET['id'];
-	$tpl->assign("PAGE_FUNC_SMALL_NAME","编辑作家");	
-	$sSQL = "SELECT * FROM $sTbl WHERE id=$id";
-	$aField = $db->getRecordSet($sSQL,1);		
+	if (!empty($id))
+	{
+		$tpl->assign("PAGE_FUNC_SMALL_NAME","编辑作家");	
+		$aField = artist::getArtistById($id);		
+	}
+	else 
+	{
+		$tpl->assign("PAGE_FUNC_SMALL_NAME","添加作家");
+		$aField['name'] = $_GET['name'];
+	}
+	
 	$oFCKeditor = new FCKeditor('FCKeditor1') ; 
 	$oFCKeditor->BasePath = "../js/editor/" ; 
 	$oFCKeditor->ToolbarSet = "Default" ; 
@@ -94,10 +102,10 @@ elseif($sAction == "edit" && isset($_GET['id'])) // 修改
 	$tpl->assign("aField",$aField);
 	$tpl->display("admin/artist_edit.tpl.htm");
 }
-elseif ($sAction == "editSave" && isset($_POST['id'])) // 修改保存
+elseif ($sAction == "editSave") // 修改保存
 {
 	$id = $_POST['id'];
-	if( $_POST['name'] == '' || $_POST['content'] == '' || $id == '')
+	if( $_POST['name'] == '' || $_POST['content'] == '')
 	{
 		redirect("artist.php?act=edit&id=$id",2,"姓名或者简介为空！");
 		exit;
@@ -108,19 +116,35 @@ elseif ($sAction == "editSave" && isset($_POST['id'])) // 修改保存
 	$aField['profession'] = $_POST['profession'];
 	$aField['eprofession'] = $_POST['eprofession'];
 	$aField['birthday'] = $_POST['birthday'];
-	$aField['artistCode'] = $_POST['artistCode'];
 	$aField['description'] = $_POST['content'];
 	$aField['edescription'] = $_POST['econtent'];
 	$aField['addDate'] = date("Y-m-d H:i:s");	
-		
-	if($db->update($sTbl,$aField,"id=$id"))
+	//update
+	if ($id > 0)
 	{
-		redirect("artist.php?act=listAll",2,"修改成功！");	
+		if($db->update($sTbl,$aField,"id=$id"))
+		{
+			redirect("artist.php?act=listAll",2,"修改成功！");	
+		}
+		else 
+		{
+			redirect("artist.php?act=edit&id=".$id,2,"修改失败！");
+		}	
 	}
+	//insert
 	else 
 	{
-		redirect("artist.php?act=edit&id=".$id,2,"修改失败！");
-	}	
+		//计算作者编码（+1即可）
+		$aField['artistCode'] = artist::getMaxCode()+1;
+		if($db->insert($sTbl,$aField))
+		{
+			redirect("artist.php?act=edit",2,"添加成功！");
+		}
+		else 
+		{
+			redirect("artist.php?act=edit",2,"添加失败！");
+		}	
+	}
 }
 elseif ($sAction == "listAll")  // 列表
 {

3718e80278e96e8e5b461c5c8efff917bc86baf7
hayley.fc <hayley.fc@9714652a-0c74-11de-97d8-e990b0f60ea0>
2009-04-13 06:50:47 +0000

    支持一副作品对应多个作者的模式。

--- trunk/code/class/work.lib.php
+++ trunk/code/class/work.lib.php
@@ -22,10 +22,21 @@ class work
 		global $db;
 		$start = !empty($params['start'])?$params['start']:0;
 		$pagesize = !empty($params['pagesize'])?$params['pagesize']:8;
-		$sSQL = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate, a.name as artist_name, a.ename as artist_ename FROM work as w left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id where w.status!='3' order by w.rank asc limit ".$start.",".$pagesize;
+		$sSQL = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate FROM work as w left join price as p on w.price=p.id where w.status!='3' order by w.rank asc limit ".$start.",".$pagesize;
 		$cSQL = "select count(*) as num from work where status!='3'";
 		//echo $sSQL;
-		$res['data'] = $db->getRecordSet($sSQL);
+		$wl = $db->getRecordSet($sSQL);
+		if (!empty($wl))
+		{
+			foreach ($wl as $k=>&$v)
+			{ 
+				$sql = "select GROUP_CONCAT(a.name) as artist_name, GROUP_CONCAT(a.ename) as artist_ename from artist a where a.artistCode in (".$v['artistCode'].")";
+				$rs =  $db->getRecordSet($sql, 1);
+				$v['artist_name'] = $rs['artist_name'];
+				$v['artist_ename'] = $rs['artist_ename'];
+			}
+		}
+		$res['data'] = $wl;
 		$rCnt = $db->getRecordSet($cSQL);
 		$res['start'] = $start;
 		$res['pagesize'] = $pagesize;
@@ -36,37 +47,57 @@ class work
 	function searchWorkList($params=array())
 	{
 		global $db;
-		if (!empty($params['vl']))
+		if (!empty($params['vl']))//头部搜索
 		{
-			$sql_tail .= " and (p.price_name like '%".$params['vl'] ."%' or p.price_ename like '%".$params['vl']. "%' ";
-			$sql_tail .= " or a.name like '%".$params['vl']."%'";
-			$sql_tail .= " or UPPER(replace(a.ename, ' ', '')) like '%".strtoupper(preg_replace('/(\s+)/', '', $params['vl']))."%'";
-			$sql_tail .= " or w.age='".$params['vl']."'";
-			$sql_tail .= " or c.name  like '%".$params['vl'] ."%' or c.name  like '%".$params['vl'] ."%')";
+			//判断搜索是否是作者
+			$sl = "select a.id from artist a where "."a.name like '%".$params['vl']."%'"." or UPPER(replace(a.ename, ' ', '')) like '%".strtoupper(preg_replace('/(\s+)/', '', $params['vl']))."%'";
+			$rt = $db->getRecordSet($sl, 1);
+			if (!empty($rt))
+			{
+				$sql_tail .= " and w.artistCode like '%".$rt['id']."%'";
+			}
+			else 
+			{
+				$sql_tail .= " and (p.price_name like '%".$params['vl'] ."%' or p.price_ename like '%".$params['vl']. "%' ";
+				//$sql_tail .= " or a.name like '%".$params['vl']."%'";
+				//$sql_tail .= " or UPPER(replace(a.ename, ' ', '')) like '%".strtoupper(preg_replace('/(\s+)/', '', $params['vl']))."%'";
+				$sql_tail .= " or w.age='".$params['vl']."'";
+				$sql_tail .= " or c.name  like '%".$params['vl'] ."%' or c.name  like '%".$params['vl'] ."%')";
+			}
 		}
-		if (!empty($params['price']))
+		else //搜索框搜索
 		{
-			$sql_tail .= " and p.id=".$params['price'];
+			if (!empty($params['artist']) || !empty($params['eartist']))
+			{
+				$sl = "select a.id from artist a where "."a.name like '%".$params['artist']."%'"." or UPPER(replace(a.ename, ' ', '')) like '%".strtoupper(preg_replace('/(\s+)/', '', $params['eartist']))."%'";
+				$rt = $db->getRecordSet($sl, 1);
+				if (empty($rt))
+				{
+					return false;
+				}
+				else 
+				{
+					$sql_tail .= " and w.artistCode like '%".$rt['id']."%'";
+				}
+			}
+			else 
+			{
+				if (!empty($params['price']))
+				{
+					$sql_tail .= " and p.id=".$params['price'];
+				}
+				if (!empty($params['age']))
+				{
+					$sql_tail .= " and w.age=".$params['age'];
+				}
+				if (!empty($params['cate']))
+				{
+					$sql_tail .= " and w.cID=".$params['cate'];
+				}
+			}				
 		}
-		if (!empty($params['artist']))
-		{
-			$sql_tail .= " and a.name like '%".$params['artist']."%'";
-		}
-		if (!empty($params['eartist']))
-		{
-			$sql_tail .= " and UPPER(replace(a.ename, ' ', '')) like '%".strtoupper(preg_replace('/(\s+)/', '', $params['eartist']))."%'";
-		}
-		if (!empty($params['age']))
-		{
-			$sql_tail .= " and w.age=".$params['age'];
-		}
-		if (!empty($params['cate']))
-		{
-			$sql_tail .= " and w.cID=".$params['cate'];
-		}
-		
-		$sql = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate, a.name as artist_name, a.ename as artist_ename FROM work as w left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id left join category c on w.cID=c.id where w.status!='3' ";
-		$sql_count = "SELECT count(*) as num FROM work as w left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id left join category c on w.cID=c.id where w.status!='3' ";
+		$sql = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate FROM work as w left join price as p on w.price=p.id left join category c on w.cID=c.id where w.status!='3' ";
+		$sql_count = "SELECT count(*) as num FROM work as w left join price as p on w.price=p.id left join category c on w.cID=c.id where w.status!='3' ";
 		
 		$start = !empty($params['start'])?$params['start']:0;
 		$pagesize = !empty($params['pagesize'])?$params['pagesize']:8;
@@ -74,36 +105,18 @@ class work
 		$sql = $sql.$sql_tail." order by w.rank asc limit ".$start.",".$pagesize;
 		$sql_count .= $sql_tail;
 		
-		$res['data'] = $db->getRecordSet($sql);
-		$rCnt = $db->getRecordSet($sql_count);
-		$res['start'] = $start;
-		$res['pagesize'] = $pagesize;
-		$res['count'] = $rCnt[0]['num'];
-		return $res;
-	}
-	
-	function searchWorkList2($params=array())
-	{
-		global $db;
-		if (!empty($params['vl']))
+		$wl = $db->getRecordSet($sql);
+		if (!empty($wl))
 		{
-			$sql_tail .= " and (p.price_name like '%".$params['vl'] ."%' or p.price_ename like '%".$params['vl']. "%' ";
-			$sql_tail .= " or a.name like '%".$params['vl']."%'";
-			$sql_tail .= " or UPPER(replace(a.ename, ' ', '')) like '%".strtoupper(preg_replace('/(\s+)/', '', $params['vl']))."%'";
-			$sql_tail .= " or w.age='".$params['vl']."'";
-			$sql_tail .= " or c.name  like '%".$params['vl'] ."%' or c.name  like '%".$params['vl'] ."%')";
+			foreach ($wl as $k=>&$v)
+			{ 
+				$sql2 = "select GROUP_CONCAT(a.name) as artist_name, GROUP_CONCAT(a.ename) as artist_ename from artist a where a.artistCode in (".$v['artistCode'].")";
+				$rs2 =  $db->getRecordSet($sql2, 1);
+				$v['artist_name'] = $rs2['artist_name'];
+				$v['artist_ename'] = $rs2['artist_ename'];
+			}
 		}
-		
-		$sql = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate, a.name as artist_name, a.ename as artist_ename FROM work as w left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id left join category c on w.cID=c.id where w.status!='3' ";
-		$sql_count = "SELECT count(*) as num FROM work as w left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id left join category c on w.cID=c.id where w.status!='3' ";
-		
-		$start = !empty($params['start'])?$params['start']:0;
-		$pagesize = !empty($params['pagesize'])?$params['pagesize']:8;
-		
-		$sql = $sql.$sql_tail." order by w.rank asc limit ".$start.",".$pagesize;
-		$sql_count .= $sql_tail;
-		
-		$res['data'] = $db->getRecordSet($sql);
+		$res['data'] = $wl;
 		$rCnt = $db->getRecordSet($sql_count);
 		$res['start'] = $start;
 		$res['pagesize'] = $pagesize;
@@ -126,6 +139,10 @@ class work
 		$res_less = $db->getRecordSet($sql_less);
 		if ($res)
 		{
+			$sl = "select GROUP_CONCAT(a.name) as artist_name, GROUP_CONCAT(a.ename) as artist_ename from artist a where a.artistCode in (".$res['artistCode'].")";
+			$rs =  $db->getRecordSet($sl, 1);
+			$res['artist_name'] = $rs['artist_name'];
+			$res['artist_ename'] = $rs['artist_ename'];
 			$res[0][less_id] = $res_less[0]['id'];
 			$res[0][more_id] = $res_more[0]['id'];
 			return $res[0];
@@ -139,9 +156,20 @@ class work
 	function getWorkbyIDs($ids)
 	{
 		global $db;
-		$sql = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate, a.name as artist_name, a.ename as artist_ename FROM work as w left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id where w.id in ($ids) and w.status!='3'";
+		$sql = "SELECT w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate FROM work as w left join price as p on w.price=p.id where w.id in ($ids) and w.status!='3'";
 		
 		$res = $db->getRecordSet($sql);
+		
+		if (!empty($res))
+		{
+			foreach ($res as $k=>$v)
+			{
+				$sl = "select GROUP_CONCAT(a.name) as artist_name, GROUP_CONCAT(a.ename) as artist_ename from artist a where a.artistCode in (".$v['artistCode'].")";
+				$rs =  $db->getRecordSet($sl, 1);
+				$v['artist_name'] = $rs['artist_name'];
+				$v['artist_ename'] = $rs['artist_ename'];
+			}
+		}
 		return $res;
 		
 	}
@@ -150,9 +178,19 @@ class work
 	{
 		global $db;
 		
-		$sSQL = "SELECT w.id, w.name, w.ename, w.cID, w.price, w.age, w.artistCode, w.picCode, w.addDate, a.name as artist_name, a.ename as artist_ename FROM work as w left join artist as a on a.artistCode=w.artistCode where w.status='2' order by w.addDate desc limit 0, 5";
+		$sSQL = "SELECT w.id, w.name, w.ename, w.cID, w.price, w.age, w.artistCode, w.picCode, w.addDate FROM work as w where w.status='2' order by w.addDate desc limit 0, 5";
 		//echo $sSQL;
 		$res = $db->getRecordSet($sSQL);
+		if (!empty($res))
+		{
+			foreach ($res as $k=>$v)
+			{
+				$sl = "select GROUP_CONCAT(a.name) as artist_name, GROUP_CONCAT(a.ename) as artist_ename from artist a where a.artistCode in (".$v['artistCode'].")";
+				$rs =  $db->getRecordSet($sl, 1);
+				$v['artist_name'] = $rs['artist_name'];
+				$v['artist_ename'] = $rs['artist_ename'];
+			}
+		}
 		return $res;
 	}
 	function getFavorite($params=array())
@@ -160,10 +198,21 @@ class work
 		global $db;
 		$start = !empty($params['start'])?$params['start']:0;
 		$pagesize = !empty($params['pagesize'])?$params['pagesize']:8;
-		$sSQL = "SELECT f.workID, f.addDate as faddDate, w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate, a.name as artist_name, a.ename as artist_ename FROM workfavorite as f left join work as w on f.workID=w.id left join artist as a on a.artistCode=w.artistCode left join price as p on w.price=p.id where f.userID='".$params['userid']."' and w.status!='3' order by f.addDate desc limit ".$start.",".$pagesize;
+		$sSQL = "SELECT f.workID, f.addDate as faddDate, w.id, w.name, w.ename, w.cID, w.age, p.price_name as price, p.price_ename as eprice, w.artistCode, w.picCode, w.addDate FROM workfavorite as f left join work as w on f.workID=w.id left join price as p on w.price=p.id where f.userID='".$params['userid']."' and w.status!='3' order by f.addDate desc limit ".$start.",".$pagesize;
 		$cSQL = "select count(*) as num from workfavorite as f left join work as w on f.workID=w.id where f.userID='".$params['userid']."' and w.status!='3' ";
 		//echo $sSQL;
-		$res['data'] = $db->getRecordSet($sSQL);
+		$wl = $db->getRecordSet($sSQL);
+		if (!empty($wl))
+		{
+			foreach ($wl as $k=>&$v)
+			{ 
+				$sql = "select GROUP_CONCAT(a.name) as artist_name, GROUP_CONCAT(a.ename) as artist_ename from artist a where a.artistCode in (".$v['artistCode'].")";
+				$rs =  $db->getRecordSet($sql, 1);
+				$v['artist_name'] = $rs['artist_name'];
+				$v['artist_ename'] = $rs['artist_ename'];
+			}
+		}
+		$res['data'] = $wl;
 		$rCnt = $db->getRecordSet($cSQL);
 		$res['start'] = $start;
 		$res['pagesize'] = $pagesize;
